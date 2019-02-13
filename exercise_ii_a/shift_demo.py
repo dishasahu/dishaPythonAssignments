73ec8ca6ce76fdc5a4abec953d093bae70d4b7c4
dishasahu <sahudisha07@gmail.com>
2019-02-13 11:57:03 +0530

    successfull added

---
+++ exercise_ii_a/shift_demo.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+num1=int(input("Enter your First Number:"))
+num=num1<<1
+print("After Left shift: {}".format(num))
+product=num1*2
+print("product is: {}".format(product))
+num=num1>>1
+print("After right shift: {}".format(num))
+division=num1/2
+print("division is: {}".format(division))
